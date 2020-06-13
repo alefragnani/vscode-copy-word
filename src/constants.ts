0ebc8b5078031ae35f1e2f3c94df71dfef612950
Alessandro Fragnani <alefragnani@hotmail.com>
2020-06-13 14:19:53 -0300

    Extracted Refactored

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,7 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { registerCommands } from "./commands";
-
-export function activate() {
-    
-    registerCommands();
+export const enum Operations {
+    Copy = "copy",
+    Cut = "cut"
 }
